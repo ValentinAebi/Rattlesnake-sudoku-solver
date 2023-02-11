918f0c91e080d9cc9c19125523081c46deb720a0
Valentin Aebi <valentin.aebi@epfl.ch>
2023-02-11 16:09:05 +0100

    Setup

---
+++ run.py
@@ -0,0 +1,16 @@
+# Script to run the compiler on all Rattlesnake files in src directory
+
+from os import listdir, system
+
+compiler_cmd = "run"
+src_dir = "src"
+rattlesnake_ext = ".rsn"
+jar_file = "Rattlesnake.jar"
+
+if __name__ == "__main__":
+    sources = listdir(src_dir)
+    rsn_sources = list(filter(lambda name: name.endswith(rattlesnake_ext), sources))
+    for i in range(len(rsn_sources)):
+        rsn_sources[i] = f"{src_dir}/{rsn_sources[i]}"
+    cmd = f"java -jar {jar_file} {compiler_cmd} {' '.join(rsn_sources)}"
+    system(cmd)
